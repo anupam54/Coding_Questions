9a8c40867a6a41808f494b026d50cc60e52b006c
Anupam Kumar Thakur <54679694+anupam54@users.noreply.github.com>
2022-01-23 21:34:28 +0530

    Magical Sticks

---
+++ 1371A.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int
+main ()
+{
+  
+int T;
+  
+cin >> T;
+  
+while (T--)
+    {
+      
+int n;
+      
+cin >> n;
+      
+ 
+cout << int (ceil (n / 2.0)) << endl;
+    
+} 
+return 0;
+
+}
