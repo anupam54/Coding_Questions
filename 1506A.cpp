669a98d5b87aa6c32fc871be0567a82a2bcea47a
Anupam Kumar Thakur <54679694+anupam54@users.noreply.github.com>
2022-03-08 23:56:40 +0530

    Strange Table

---
+++ 1506A.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+void solve(){
+    ll n,m,x;
+    cin>>n>>m>>x;
+    ll a=(x-1)/n;
+    ll b=(x-1)%n;
+    ll ans=(b)*m + (a+1);
+    cout<<ans<<"\n";
+}  
+int main(){
+    ios::sync_with_stdio(0);
+            cin.tie(0);
+            cout.tie(0);
+            cout<<fixed;
+            cout<<setprecision(10);
+    //        freopen("timber_input.txt", "r", stdin);
+    //        freopen("timber_output.txt", "w", stdout);
+            int t=1;
+            cin>>t;
+            for(int i=1;i<=t;i++){
+            //    cout<<"Case #"<<i<<": ";  
+                solve();
+    }
+    return 0;
+}
