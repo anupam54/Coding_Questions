8bf2a7043afcbd7f757b4a7b4257e2ecef071c41
Anupam Kumar Thakur <54679694+anupam54@users.noreply.github.com>
2022-03-12 23:31:53 +0530

    Gregor and Cryptography

---
+++ 1549A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--){
+        int p;
+        cin>>p;
+        p--;
+        cout<<p/2<<" "<<p<<endl;
+    }
+    return 0;
+}
