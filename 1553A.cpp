92d75f6a41704d6240563f389c742f915000adfc
Anupam Kumar Thakur <54679694+anupam54@users.noreply.github.com>
2022-03-23 19:01:35 +0530

    Digits Sum

---
+++ 1553A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        cin>>n;
+        cout<<(n+1)/10<<endl;
+    }
+    return 0;
+}
