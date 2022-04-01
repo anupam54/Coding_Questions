6881fcad831c2c2efb5f182dbcdefeb5b02771b2
Anupam Kumar Thakur <54679694+anupam54@users.noreply.github.com>
2022-04-01 16:08:52 +0530

    Arena

---
+++ 1487A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++) cin>>a[i];
+        int mini=*min_element(a.begin(),a.end());
+        int total=0;
+        for(int i=0;i<n;i++){
+            if(a[i]!=mini) total++;
+        }
+        cout<<total<<"\n";
+    }
+    return 0;
+}
